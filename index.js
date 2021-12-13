c0e2eb400ade8c74843b7fcba106d3a5280f981f
Yanfox <yan@mail.ru>
2021-12-13 08:56:21 +0700

    21

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'pages');
 
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'pages','index.html'));
+    res.render('index');
 })
 
 app.listen(PORT, ()=>{
